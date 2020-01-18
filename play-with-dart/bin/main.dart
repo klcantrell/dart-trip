dc5fd3faf4bc1112640cb5a469ab84df4c6f4540
klcantrell <cantrellkalalau@gmail.com>
2020-01-18 12:45:01 -0500

    play-with-dart: Add path package. Save generated type def file.

--- play-with-dart/bin/main.dart
+++ play-with-dart/bin/main.dart
@@ -3,40 +3,13 @@ import 'dart:convert';
 
 import 'package:play_with_dart/play_with_dart.dart';
 
-void handleOnChange(FileSystemEvent e) {
-  var modifyEvent = e as FileSystemModifyEvent;
-  if (modifyEvent.contentChanged) {
-    var file = File(modifyEvent.path);
-    var typeDefFileContent = processCss(file.readAsStringSync());
-    print(typeDefFileContent);
-  }
-}
-
-String processCss(String content) {
-  return RegExp(r'\.\w+', multiLine: true)
-      .allMatches(content)
-      .map(extractMatch)
-      .map(removeDot)
-      .map(typeDefOfRule)
-      .fold('', createTypeDefFile);
-}
-
-void handleOnCommand(String e) {
-  if (e == 'quit()') {
-    exit(0);
-  }
-}
-
 void main(List<String> arguments) {
-  stdout.writeln('Edit a CSS file in this directory or subdirectories...');
-  stdout.writeln('TypeScript type defs shall be generated');
-  stdout.writeln();
-  stdout.writeln('Enter quit() to exit');
-  stdout.writeln();
+  printOpeningInstructions();
 
   Directory('.')
       .watch(events: FileSystemEvent.modify, recursive: true)
-      .listen(handleOnChange);
+      .where(isCssFile)
+      .listen(handleChangedCss);
 
-  stdin.transform(utf8.decoder).map((s) => s.trim()).listen(handleOnCommand);
+  stdin.transform(utf8.decoder).map(trimConsoleInput).listen(handleOnCommand);
 }
--- play-with-dart/lib/play_with_dart.dart
+++ play-with-dart/lib/play_with_dart.dart
@@ -1,4 +1,50 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+var trimConsoleInput = (String input) => input.trim();
+
 var removeDot = (String rule) => rule.substring(1);
+
 var extractMatch = (RegExpMatch match) => match.group(0);
+
 var typeDefOfRule = (String ruleName) => 'export const ${ruleName}: string;\n';
+
 var createTypeDefFile = (content, typeDef) => '${content}${typeDef}';
+
+var isCssFile = (FileSystemEvent e) => e.path.endsWith('.css');
+
+void handleChangedCss(FileSystemEvent e) {
+  var modifyEvent = e as FileSystemModifyEvent;
+  if (modifyEvent.contentChanged) {
+    var cssFile = File(modifyEvent.path);
+    var cssFilename = p.basename(cssFile.path);
+    var typeDefFileContent = processCss(cssFile.readAsStringSync());
+
+    var typeDefFile = File('${p.join(cssFile.parent.path, cssFilename)}.d.ts');
+    typeDefFile.writeAsStringSync(typeDefFileContent);
+    print('Saved TS type def for ${cssFilename}');
+  }
+}
+
+String processCss(String content) {
+  return RegExp(r'\.\w+', multiLine: true)
+      .allMatches(content)
+      .map(extractMatch)
+      .map(removeDot)
+      .map(typeDefOfRule)
+      .fold('', createTypeDefFile);
+}
+
+void handleOnCommand(String e) {
+  if (e == 'quit()') {
+    exit(0);
+  }
+}
+
+void printOpeningInstructions() {
+  stdout.writeln('Edit a CSS file in this directory or subdirectories...');
+  stdout.writeln('TypeScript type defs shall be generated');
+  stdout.writeln();
+  stdout.writeln('Enter quit() to exit');
+  stdout.writeln();
+}
