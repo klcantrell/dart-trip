798a8b8aabec4dd2dd39d9c00e8f143fbfff9c03
klcantrell <cantrellkalalau@gmail.com>
2020-06-28 21:29:52 -0400

    max-flutter/personal_expenses: Add Transaction class and start card widget

--- max-flutter/personal_expenses/lib/main.dart
+++ max-flutter/personal_expenses/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './trasaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +16,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Grocieries',
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,10 +49,64 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ]),
-          Card(
-            child: Text('List of transactions'),
-            elevation: 5,
-          ),
+          Flex(
+            direction: Axis.horizontal,
+            children: [
+              Expanded(
+                child: Column(
+                  children: transactions
+                      .map((tx) => Card(
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  margin: EdgeInsets.symmetric(
+                                    vertical: 10,
+                                    horizontal: 15,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.purple,
+                                      width: 2,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    tx.amount.toString(),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.purple,
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      tx.title,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                        color: Colors.purple,
+                                      ),
+                                    ),
+                                    Text(
+                                      tx.date.toString(),
+                                      style: TextStyle(
+                                        color: Colors.grey.shade500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            elevation: 5,
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
