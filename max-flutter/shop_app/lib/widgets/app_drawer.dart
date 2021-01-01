462f7861d502b212f95134bef2a01cbe847b3d9c
klcantrell <cantrellkalalau@gmail.com>
2020-12-31 20:29:56 -0500

    max-flutter/shop: apply custom page transition to all routes

--- max-flutter/shop_app/lib/widgets/app_drawer.dart
+++ max-flutter/shop_app/lib/widgets/app_drawer.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_products.screen.dart';
-import '../helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -29,11 +28,8 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.payment),
             title: Text('Orders'),
             onTap: () {
-              Navigator.of(context).pushReplacement(
-                CustomRoute(
-                  builder: (ctx) => OrdersScreen(),
-                ),
-              );
+              Navigator.of(context)
+                  .pushReplacementNamed(OrdersScreen.routeName);
             },
           ),
           Divider(),
