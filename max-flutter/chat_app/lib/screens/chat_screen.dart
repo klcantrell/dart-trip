77d46e2b4c0c7e39baad2d98d4fa937864cd197c
klcantrell <cantrellkalalau@gmail.com>
2021-02-11 22:43:40 -0500

    max-flutter/chat: add push notifications

--- max-flutter/chat_app/lib/screens/chat_screen.dart
+++ max-flutter/chat_app/lib/screens/chat_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/chat/messages.dart';
 import '../widgets/chat/new_message.dart';
 
 const collectionName = 'chat/Jyz4iUHejJgylRVHb0hd/messages';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    _firebaseMessaging.requestNotificationPermissions();
+    _firebaseMessaging.configure(
+      onMessage: (message) {
+        print('onMessage');
+        print(message);
+        return;
+      },
+      onLaunch: (message) {
+        print('onLaunch');
+        print(message);
+        return;
+      },
+      onResume: (message) {
+        print('onResume');
+        print(message);
+        return;
+      },
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
